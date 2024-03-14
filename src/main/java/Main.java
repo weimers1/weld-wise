e7d2921563344de327304b4ae18d0949e936e281
unknown <samweimer7@gmail.com>
2024-03-14 15:19:36 -0400

    restructuring files

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+package main.java;
+
+import components.java.Query;
+
+public class Main {
+    public static void main(String[] args) {
+        Query query = new Query();
+
+        System.out.println(query.execute("""
+            select
+                test1,
+                test2
+            from
+                TEST_TABLE
+        """));
+    }
+}
